34ad7a1c971d35a64d0d357de46136754f63eeae
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-10-07 01:01:49 +0200

    initial commit

---
+++ src/app/flipdots/page.tsx
@@ -0,0 +1,16 @@
+export default function FlipDots() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="border-2 border-white w-[840px] h-[280px] bg-black flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white text-5xl font-bold">Leaderboard</h1>
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-white text-4xl font-bold">1. Jarvin</h2>
+            <h2 className="text-white text-4xl font-bold">2. Jarvin</h2>
+            <h2 className="text-white text-4xl font-bold">3. Jarvin</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
